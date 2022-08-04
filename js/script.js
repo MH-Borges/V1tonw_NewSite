2dcdf2cbbdaea68911dd32d3739026b446ff638d
Matheus Henrique Borges <matheush.borges@hotmail.com>
2022-08-04 12:16:56 -0300

    Some effects

--- js/script.js
+++ js/script.js
@@ -1,48 +1,79 @@
 colorArray = [ '#FF4500', '#36B7FF' ];
+const random = Math.floor(Math.random()* colorArray.length);
 
 window.onload = function(){
-    const random = Math.floor(Math.random()* colorArray.length);
     document.body.style.background = `${colorArray[random]}`;
     document.querySelector('.block_vetor').style=`left:${Math.floor(Math.random() * 64)}vw`;
 
     if(colorArray[random] == '#FF4500'){
-        
         const texts = document.querySelectorAll('.marqueeText');
         for (const text of texts) { text.classList.remove('blue'); }
-
     }
     else{
-
         const texts = document.querySelectorAll('.marqueeText');
         for (const text of texts) { text.classList.add('blue'); }
 
         const links = document.querySelectorAll('.link');
         for (const link of links) { link.classList.add('darkRed'); }
+    }
+}
+
+window.addEventListener('scroll',() => {
+    if(window.scrollY < (document.documentElement.clientHeight - 35)){
+        document.querySelector('.projetos').classList.remove('fixed');
+        document.querySelector('.blockImgs').classList.remove('imgs');
 
     }
-   
-    window.addEventListener('scroll',() => {
-        if(window.scrollY < 669){
-            document.querySelector('.projetos').classList.remove('fixed');
-            document.querySelector('.blockImgs').classList.remove('imgs');
+    if(window.scrollY >= (document.documentElement.clientHeight - 35)){
+        document.querySelector('.projetos').classList.add('fixed');
+        document.querySelector('.blockImgs').classList.add('imgs');
+    }
 
+});
+
+function clickPhoto() {
+    document.querySelector('.ficha').style.opacity = '1';
+    document.querySelector('.ficha').style.position = 'relative';
+
+    const projects = document.querySelectorAll('.project');
+    for (const project of projects) {  project.remove(); }
+
+    var nomeProjeto = document.querySelector(".nomeProjeto").textContent;
+    
+    let text = "";
+    let text2 = "";
+    let i = 0;
+
+    if(colorArray[random] == '#36B7FF'){
+        while (i < 4) {
+            text += `<li class='marqueeText project blue' style="flex: 0 0 auto; display: block;"> ${ nomeProjeto } </li>`;
+            text2 += `<li class='marqueeText project infiniteslide_clone blue' style="flex: 0 0 auto; display: block;"> ${ nomeProjeto } </li>`;
+            
+            i++;
+            }
+    }else{
+        while (i < 4) {
+        text += `<li class='marqueeText project' style="flex: 0 0 auto; display: block;"> ${ nomeProjeto } </li>`;
+        text2 += `<li class='marqueeText project infiniteslide_clone' style="flex: 0 0 auto; display: block;"> ${ nomeProjeto } </li>`;
+        
+        i++;
         }
-        if(window.scrollY >= 670){
-            document.querySelector('.projetos').classList.add('fixed');
-            document.querySelector('.blockImgs').classList.add('imgs');
-        }
-    });
+    }
+    document.querySelector(".scroll1").innerHTML = text + text2;
 
 }
 
-
 $(function () {
     $('.scroll').infiniteslide({
-        speed: 100,
+    });
+
+    $('.scroll1').infiniteslide({
+        speed: 150,
         pauseonhover: false
     });
 
-
-    $('.scroll1').infiniteslide({
+    $('.scroll2').infiniteslide({
+        pauseonhover: false
     });
+
 });
